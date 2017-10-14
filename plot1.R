6fc60118c329cf9864d39cacbae7c9d25e991b8b
Joe Lee <kamshee@gmail.com>
2017-10-13 20:25:56 -0500

    Updated R script files with streamlined code

--- plot1.R
+++ plot1.R
@@ -8,32 +8,24 @@ fileUrl <-
  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl, "dataset.zip")
 unzip("dataset.zip")
-# ? Extract subset to cut down on processing time....
-        #data1 <- read.table("./EDAwk1project/household_power_consumption.txt")
-        #data2 <- read.csv("./EDAwk1project/household_power_consumption.txt")
-data3 <- fread("./household_power_consumption.txt")
-   # coerces '?' values to all character, so creates more work with as.numeric
-##powerdata <- read.csv("household_power_consumption.txt", header = TRUE, sep = ';')
 
+data2 <- fread("./household_power_consumption.txt",
+        header = TRUE,na.strings = c("?","","NA"),
+        colClasses = c(rep("character", 2), rep("numeric", 7)))
+
+## Subset by data and convert to Date format
+data3 <- subset(data2, Date=="1/2/2007" | Date=="2/2/2007")
+##data3 <- data2[data2$Date %in% c("1/2/2007","2/2/2007"),]
 data3$Date <- as.Date(data3$Date, "%d/%m/%Y")
-firstday <- subset(data3,Date=="2007-2-1")
-secondday <- subset(data3,Date=="2007-2-2")
-totalday<-rbind(firstday,secondday)
 
-data<-select(totalday, -Global_intensity)
-data$Global_active_power<-as.numeric(data$Global_active_power)
-data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
-data$Voltage<-as.numeric(data$Voltage)
-data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
-data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
-
-datetime<-paste(data$Date,data$Time)
-datetime<-strptime(datetime,"%Y-%m-%d %H:%M:%S")
-datetime<-as.data.frame(datetime)
-data <- cbind(data,datetime)
+## Create and add datetime column
+datetime <-strptime(paste(data3$Date,data3$Time, sep=" "),"%Y-%m-%d %H:%M:%S")
+data3 <- cbind(data3,datetime)
 
 ## Create Plot 1
-with(data,hist(Global_active_power,main="Global Active Power",
+par(mfrow=c(1,1))
+## windows(480,480)
+with(data3,hist(Global_active_power,main="Global Active Power",
                xlab="Global Active Power (kilowatts)", col="red"))
-dev.copy(png,file="plot1.png") ## default pixel width 480 x height 480
+dev.copy(png,file="plot1.png") ## default pixel (width=480, height=480)
 dev.off()
--- plot4.R
+++ plot4.R
@@ -8,28 +8,21 @@ fileUrl <-
         "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl, "dataset.zip")
 unzip("dataset.zip")
-data3 <- fread("./household_power_consumption.txt")
+data2 <- fread("./household_power_consumption.txt",
+               header = TRUE,na.strings = c("?","","NA"),
+               colClasses = c(rep("character", 2), rep("numeric", 7)))
 
+## Subset by data and convert to Date format
+data3 <- subset(data2, Date=="1/2/2007" | Date=="2/2/2007")
 data3$Date <- as.Date(data3$Date, "%d/%m/%Y")
-firstday <- subset(data3,Date=="2007-2-1")
-secondday <- subset(data3,Date=="2007-2-2")
-totalday<-rbind(firstday,secondday)
 
-data<-select(totalday, -Global_intensity)
-data$Global_active_power<-as.numeric(data$Global_active_power)
-data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
-data$Voltage<-as.numeric(data$Voltage)
-data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
-data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
-
-datetime<-paste(data$Date,data$Time)
-datetime<-strptime(datetime,"%Y-%m-%d %H:%M:%S")
-datetime<-as.data.frame(datetime)
-data <- cbind(data,datetime)
+## Create and add datetime column
+datetime <-strptime(paste(data3$Date,data3$Time, sep=" "),"%Y-%m-%d %H:%M:%S")
+data3 <- cbind(data3,datetime)
 
 par(mfcol=c(2,2))
 
-with(data,{
+with(data3,{
         plot(datetime,Global_active_power, type="l", lty=1,
                 xlab="", ylab="Global Active Power (kilowatts)")
         plot(datetime,Sub_metering_1, type="l", lty=1,
